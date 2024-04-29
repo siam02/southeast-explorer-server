2a6ed3cf56329bcf4043cfdcc4cb8673ab7ef1c5
siam02 <siam.jaman02@gmail.com>
2024-04-29 12:58:26 +0600

    Ping Removed

--- index.js
+++ index.js
@@ -88,9 +88,6 @@ async function run() {
             const result = await touristsSpotCollection.updateOne(filter, touristsSpot, options);
             res.send(result);
         })
-
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally {
 
