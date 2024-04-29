8158eb9938c18fb0556ef28fbfa1700b6f73f7a6
siam02 <siam.jaman02@gmail.com>
2024-04-29 12:11:13 +0600

    Tourist Spot Delete option added

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
             }
         })
 
+        app.delete('/tourists-spot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await touristsSpotCollection.deleteOne(query);
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
