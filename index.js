4dfc95a9f3f7aa28921061071fe3ef64add2ece9
siam02 <siam.jaman02@gmail.com>
2024-04-29 20:54:31 +0600

    Single Country & Update Country API route added

--- index.js
+++ index.js
@@ -97,6 +97,14 @@ async function run() {
             res.send(countries);
         })
 
+        app.get('/country/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await countryCollection.findOne(query);
+            res.send(result);
+        })
+
+
         app.post('/country', async (req, res) => {
             const newCountry = req.body;
             const result = await countryCollection.insertOne(newCountry);
@@ -109,6 +117,25 @@ async function run() {
             const result = await countryCollection.deleteOne(query);
             res.send(result);
         })
+
+        app.put('/country/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updateCountry = req.body;
+
+            const country = {
+                $set: {
+                    image: updateCountry.image,
+                    country_Name: updateCountry.country_Name,
+                    short_description: updateCountry.short_description
+                }
+            }
+
+            const result = await countryCollection.updateOne(filter, country, options);
+            res.send(result);
+        })
+
     }
     finally {
 
