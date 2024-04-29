e7314266999feae00f850a3662622566c93b2443
siam02 <siam.jaman02@gmail.com>
2024-04-29 20:31:20 +0600

    Delete Country API route added

--- index.js
+++ index.js
@@ -96,6 +96,19 @@ async function run() {
             const countries = await cursor.toArray();
             res.send(countries);
         })
+
+        app.post('/country', async (req, res) => {
+            const newCountry = req.body;
+            const result = await countryCollection.insertOne(newCountry);
+            res.send(result);
+        })
+
+        app.delete('/country/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await countryCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
